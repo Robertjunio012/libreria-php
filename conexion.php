28cd977dcac8c722abc22ab649f5c95358ad433e
Robertjunio012 <tunometecabrabaki8@gmail.com>
2026-08-06 23:39:04 -0400

    Solución de rutas y despliegue final en InfinityFree

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost';
-$dbname = 'dblibreria';
-$username = 'root';
-$password = '';
+$host = 'sql105.infinityfree.com'; 
+$dbname = 'if0_42597283_dblibreria'; 
+$username = 'if0_42597283'; 
+$password = 'D4iUk6dh5wQXXm'; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Error: " . $e->getMessage());
+    die("Error de conexión: " . $e->getMessage());
 }
 ?>
